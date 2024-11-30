654858a7cdf48f74724ce12d6c01e484e0978afd
V86N <victor-nap@hotmail.com>
2024-11-30 18:17:13 +0100

    primer commit

---
+++ controllers/PostController.js
@@ -0,0 +1,17 @@
+const Post = require("../models/Post")
+
+
+
+const PostController = {
+    async create(req,res){
+        try {
+            const newPost = await Post.create(req.body)
+        res.status(201).send({message:"New post successfully created",newProduct})
+        } catch (error) {
+            console.error(error);
+            res.status(500).send({message:"There was a problem",error})
+        } 
+    }
+}
+
+module.exports = PostController
