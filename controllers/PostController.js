ba2c88467587b9836f5f0d5505a9987f26640f15
V86N <victor-nap@hotmail.com>
2024-12-02 09:50:07 +0100

    newPost fixed

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -6,7 +6,7 @@ const PostController = {
     async create(req,res){
         try {
             const newPost = await Post.create(req.body)
-        res.status(201).send({message:"New post successfully created",newProduct})
+        res.status(201).send({message:"New post successfully created",newPost})
         } catch (error) {
             console.error(error);
             res.status(500).send({message:"There was a problem",error})
