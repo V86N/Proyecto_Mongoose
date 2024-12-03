9e0b6df9eb5c24caf5776cf2098222564e4cc7ec
fullstackLuis91 <luis_rv91@hotmail.com>
2024-12-03 15:17:54 +0100

    proyecto atualizado

--- routes/users.js
+++ routes/users.js
@@ -5,5 +5,6 @@ const {authentication} = require("../middlewares/authentication")
 
 router.post("/", UserController.register)
 router.post("/login",UserController.login)
+router.post("/logout",UserController.logout)
 
 module.exports = router
